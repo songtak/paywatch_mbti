bd740fe7bef2b36e571358decfae5c0c93b7a007
tak.song-at-585251656769 <>
2023-01-06 17:28:33 +0900

    이미지 다운로드

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useParams } from "react-router";
 import { useNavigate } from "react-router";
 import { rabbitYearResultList } from "../model/Rabbit";
@@ -31,6 +31,8 @@ import _ from "lodash";
 const ResultPage = () => {
   const navigate = useNavigate();
   const { mbti } = useParams();
+  const resultImageRef = useRef<HTMLDivElement>(null);
+
   const [isOpenShareToast, setIsOpenShareToast] = useState<boolean>(false);
 
   const result: any = rabbitYearResultList.find(
@@ -38,6 +40,25 @@ const ResultPage = () => {
   );
 
   // console.log("result", result);
+
+  // const handleClickDownload = () => {
+  //   //
+  // };
+
+  /** 저장하기 */
+  const handleClickDownload = useCallback(() => {
+    if (resultImageRef.current === null) {
+      // loadingStore.endLoading();
+      return;
+    }
+    domtoimage.toJpeg(resultImageRef.current).then((dataUrl: string) => {
+      // setRegisterCms(dataUrl);
+      var link = document.createElement("a");
+      link.download = "2023_rabbit_paywatch.jpeg";
+      link.href = dataUrl;
+      link.click();
+    });
+  }, [resultImageRef]);
 
   const handleClickShare = () => {
     if (navigator.share) {
@@ -82,13 +103,20 @@ const ResultPage = () => {
       <div className="main_wrapper">
         <div className="main_header"></div>
         <div className="main_body_wrapper">
-          <div>
-            <div className="result_sub_title">{result?.subTitle}</div>
-            <div className="result_title">{result?.title}</div>
-          </div>
-          <div>
+          <div
+            ref={resultImageRef}
+            style={{
+              backgroundColor: "rgb(246, 248, 188)",
+              paddingTop: "24px",
+            }}
+          >
             <div>
-              {/* <img
+              <div className="result_sub_title">{result?.subTitle}</div>
+              <div className="result_title">{result?.title}</div>
+            </div>
+            <div>
+              <div>
+                {/* <img
                 src={ISTJ}
                 // src={mbti?.toUpperCase()}
                 // src={`/images/mbti/${mbti}.png`}
@@ -97,71 +125,80 @@ const ResultPage = () => {
                 className=""
                 height={250}
               /> */}
-              {mbti?.toUpperCase() === "ISTJ" && (
-                <img src={ISTJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ISTP" && (
-                <img src={ISTP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ISFJ" && (
-                <img src={ISFJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ISFP" && (
-                <img src={ISFP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "INTJ" && (
-                <img src={INTJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "INTP" && (
-                <img src={INTP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "INFJ" && (
-                <img src={INFJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "INFP" && (
-                <img src={INFP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ESTJ" && (
-                <img src={ESTJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ESTP" && (
-                <img src={ESTP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ESFJ" && (
-                <img src={ESFJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ESFP" && (
-                <img src={ESFP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ENTJ" && (
-                <img src={ENTJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ENTP" && (
-                <img src={ENTP} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ENFJ" && (
-                <img src={ENFJ} alt="" className="" height={250} />
-              )}
-              {mbti?.toUpperCase() === "ENFP" && (
-                <img src={ENFP} alt="" className="" height={250} />
-              )}
-            </div>
-            <div className="result result-font">{result?.result}</div>
-            <div className="result_ad">
-              <img
-                src={AD}
-                alt=""
-                className="ad_img"
-                onClick={() => {
-                  window.open("https://www.paywatch.co.kr/");
-                }}
-              />
+                {mbti?.toUpperCase() === "ISTJ" && (
+                  <img src={ISTJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ISTP" && (
+                  <img src={ISTP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ISFJ" && (
+                  <img src={ISFJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ISFP" && (
+                  <img src={ISFP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "INTJ" && (
+                  <img src={INTJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "INTP" && (
+                  <img src={INTP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "INFJ" && (
+                  <img src={INFJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "INFP" && (
+                  <img src={INFP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ESTJ" && (
+                  <img src={ESTJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ESTP" && (
+                  <img src={ESTP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ESFJ" && (
+                  <img src={ESFJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ESFP" && (
+                  <img src={ESFP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ENTJ" && (
+                  <img src={ENTJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ENTP" && (
+                  <img src={ENTP} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ENFJ" && (
+                  <img src={ENFJ} alt="" className="" height={250} />
+                )}
+                {mbti?.toUpperCase() === "ENFP" && (
+                  <img src={ENFP} alt="" className="" height={250} />
+                )}
+              </div>
+              <div className="result result-font">{result?.result}</div>
+              <div className="result_ad">
+                <img
+                  src={AD}
+                  alt=""
+                  className="ad_img"
+                  onClick={() => {
+                    window.open("https://www.paywatch.co.kr/");
+                  }}
+                />
+              </div>
             </div>
           </div>
           <div className="main_button_wrapper">
             {/* <button className="main_button" onClick={handleClickShare}>
               토끼마을 구경가기!
             </button> */}
+            <button
+              className="main_button start"
+              onClick={() => {
+                handleClickDownload();
+              }}
+            >
+              저장하기
+            </button>
             <button
               className="main_button start"
               onClick={() => {
